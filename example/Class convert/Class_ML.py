f4fa6f3b54d5dddf288f836fae19145f27bccc93
InSeop Hwang <inseop.hwang@hotmail.com>
2022-07-01 23:56:53 +0900

    <committed details.> 1) in progress - update modeling: voting Regressor - class convert
    2) Done
    -

---
+++ example/Class convert/Class_ML.py
@@ -0,0 +1,14 @@
+
+
+
+## 머신러닝 클래스 생성
+class Machine_learning:
+
+    ## 속성 생성
+    def __init__(self, selected_ML, data, target):
+        self.selected_ML = selected_ML
+        self.data = data
+        self.target = target
+
+
+    ## 메소드 생성
