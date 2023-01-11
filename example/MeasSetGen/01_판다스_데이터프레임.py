53aba9b40cc833545d54f152cbcbfe7bfc576b7d
InSeop Hwang <inseop.hwang@hotmail.com>
2023-01-11 20:53:00 +0900

    1) bsIndexTrace update - need to ask UE team rule

--- example/MeasSetGen/01_판다스_데이터프레임.py
+++ example/MeasSetGen/01_판다스_데이터프레임.py
@@ -9,32 +9,57 @@ if __name__ == '__main__':
     data = filedialog.askopenfilename(initialdir='.txt')
     data = pd.read_csv(data, sep='\t', encoding='cp949')
     
-    ## columns name
+    ## columns name to 대문자.
     data.columns = [x.upper() for x in data.columns]
     
-        
-    # list_params = ['Mode', 'BeamStyleIndex', 'SysTxFreqIndex', 'TxpgWaveformStyle', 'TxFocusLocCm', 'NumTxElements', 'ProbeNumTxCycles', 'IsTxChannelModulationEn',
-    #                'IsPresetCpaEn', 'CpaDelayOffsetClk', 'TxPulseRle', 'TxpgWaveformLut', 'SystemPulserSel', 'VTxIndex', 'elevAperIndex']
+    list_param = ['PROBENAME', 'MODE', 'SUBMODEINDEX', 'BEAMSTYLEINDEX', 'SYSTXFREQINDEX', 'TXPGWAVEFORMSTYLE', 'TXFOCUSLOCCM', 'NUMTXELEMENTS',
+                  'PROBENUMTXCYCLES', 'ISTXCHANNELMODULATIONEN', 'ISPRESETCPAEN', 'CPADELAYOFFSETCLK', 'TXPULSERLE', 'TXPGWAVEFORMLUT', 'ELEVAPERINDEX',
+                  'SYSTEMPULSERSEL', 'VTXINDEX']
     
-    selected_data = data.loc[:, list_params]
+    selected_data = data.loc[:, list_param]
     
     ##############################
     ##### B & M mode process #####
-    ## df_sort_B = df_B_mode.sort_values(by=[df_B_mode.columns[0], df_B_mode.columns[1], df_B_mode.columns[2], df_B_mode.columns[5], df_B_mode.columns[3]], ascending=True)
-    ## B_num = df_sort_B['TxFocusLocCm'].nunique()
+    df_B_mode = selected_data.loc[(selected_data['MODE'] == 'B')]
+    df_M_mode = selected_data.loc[(selected_data['MODE'] == 'M')]
+    df_C_mode = selected_data.loc[selected_data['MODE'] == 'Cb']
+    df_D_mode = selected_data.loc[selected_data['MODE'] == 'D']
+    df_CEUS_mode = selected_data.loc[selected_data['MODE'] == 'Contrast']
     
-    df_B_mode = selected_data.loc[(selected_data['Mode'] == 'B')]
-    df_M_mode = selected_data.loc[(selected_data['Mode'] == 'M')]
-    df_C_mode = selected_data.loc[selected_data['Mode'] == 'Cb']
-    df_D_mode = selected_data.loc[selected_data['Mode'] == 'D']
-    df_CEUS_mode = selected_data.loc[selected_data['Mode'] == 'Contrast']
     
     ## 4개 모드 데이터프레임 합치기
     ## 합쳐진 데이터프레임 index reset
     df_total = pd.concat([df_B_mode, df_M_mode, df_C_mode, df_D_mode, df_CEUS_mode])                                            
     df_total = df_total.reset_index(drop=True)                                                                          
     
+    
     ## 데이터 Null --> [0]으로 변환(데이터의 정렬, groupby null 값 문제 발생)
     df_total = df_total.fillna(0)
     
-    df_total.to_csv('test_total.csv')
+    
+    ## duplicated parameter check. => dup = df.duplicated(['SysTxFreqIndex', 'TxpgWaveformStyle', 'TxFocusLocCm', 'NumTxElements', 'ProbeNumTxCycles', 'IsTxChannelModulationEn', 'TxPulseRle'], keep='first')
+    ## 중복된 parameter가 있을 경우, 제거하기.
+    ## group_param 기준으로 데이터 정렬하기
+    group_params =['ISTXCHANNELMODULATIONEN', 'SYSTXFREQINDEX', 'TXPGWAVEFORMSTYLE', 'PROBENUMTXCYCLES', 'TXPULSERLE', 'TXFOCUSLOCCM', 'NUMTXELEMENTS']
+    df_drop_dup = df_total.drop_duplicates(group_params)
+    df_sort = df_drop_dup.sort_values(by=group_params, ascending=True).reset_index()
+    
+    
+    ## groupby 로 중복 count
+    dup_count = df_drop_dup.groupby(by=group_params, as_index=False).count()                                          
+    
+    bsIndexTrace = []
+    
+    for subidx, cnt in zip(df_sort['SUBMODEINDEX'], dup_count['BEAMSTYLEINDEX']):
+        if subidx == 0 and cnt >= 2:
+            bsIndexTrace.append(15)
+        elif subidx == 1 and cnt >= 2:
+            bsIndexTrace.append(20)
+        elif subidx == 5 and cnt >= 2:
+            bsIndexTrace.append(10)
+        else:
+            bsIndexTrace.append(0)
+    
+    df_sort['bsIndexTrace'] = bsIndexTrace
+    
+    df_sort.to_csv('check_20230111.csv')
