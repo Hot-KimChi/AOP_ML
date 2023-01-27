e2ddbcc8d5888b749f5bf640c4805f4af68c1ff6
InSeop Hwang <inseop.hwang@hotmail.com>
2023-01-27 14:36:53 +0900

    1) find_origin_beamstyleindex from subidx

--- example/MeasSetGen/01_판다스_데이터프레임.py
+++ example/MeasSetGen/01_판다스_데이터프레임.py
@@ -21,8 +21,6 @@ class MeasSetgen(object):
         ## list_param, 즉 선택한 parameter만 데이터프레임.
         self.df_selected = self.data.loc[:, list_param]
         
-        self.fn_merge_df()
-        
         
     def fn_merge_df(self):
         
@@ -41,30 +39,47 @@ class MeasSetgen(object):
         df_total = df_total.fillna(0)
                 
         ## groupby 로 중복 count / df_total에 count해서 집어넣기
-        group_params =['ISTXCHANNELMODULATIONEN', 'SYSTXFREQINDEX', 'TXPGWAVEFORMSTYLE', 'PROBENUMTXCYCLES', 'TXPULSERLE', 'ELEVAPERINDEX', 'TXFOCUSLOCCM', 'NUMTXELEMENTS']
-        dup_count = df_total.groupby(by=group_params, as_index=False).count()    
+        self.group_params =['ISTXCHANNELMODULATIONEN', 'SYSTXFREQINDEX', 'TXPGWAVEFORMSTYLE', 'PROBENUMTXCYCLES', 'TXPULSERLE', 'ELEVAPERINDEX', 'TXFOCUSLOCCM', 'NUMTXELEMENTS']
+        dup_count = df_total.groupby(by=self.group_params, as_index=False).count()    
         df_total['Count'] = dup_count['BEAMSTYLEINDEX']
                 
         ## 중복된 parameter가 있을 경우, 제거하기.
-        df_drop_dup = df_total.drop_duplicates(group_params)
-                
-        ## group param에서 SUBMODEINDEX 추가하여 정렬 준비 및 정렬하기
-        sort_params = ['SUBMODEINDEX'] + group_params
-        df_sort = df_drop_dup.sort_values(by=sort_params, ascending=True).reset_index()
-         
-        self.df = df_sort
+        self.df = df_total.drop_duplicates(self.group_params)
+    
+
+    
+    def fn_findOrgIdx(self):
         
+        orgindex = []
         
+        for mode, subidx in zip(self.df['MODE'], self.df['SUBMODEINDEX']):
+            if mode == 'B' and subidx == 0:
+                orgindex.append(0)
+            elif mode == 'B' and subidx == 1:
+                orgindex.append(1)
+            elif mode == 'Cb' and subidx == 0:
+                orgindex.append(5)
+            elif mode == 'D' and subidx == 0:
+                orgindex.append(10)
+            elif mode == 'M' and subidx == 0:
+                orgindex.append(15)
+            elif mode == 'M' and subidx == 1:
+                orgindex.append(20)
+        
+        self.df['OrgIndex'] = orgindex
+        
+    
+    ## bsIndexTrace algorithm    
     def fn_bsIdx(self):
         
         bsIndexTrace = []
         
-        for subidx, cnt in zip(self.df['SUBMODEINDEX'], self.df['Count']):
-            if subidx == 0 and cnt >= 2:
+        for orgidx, cnt in zip(self.df['OrgIndex'], self.df['Count']):
+            if orgidx == 0 and cnt >= 2:
                 bsIndexTrace.append(15)
-            elif subidx == 1 and cnt >= 2:
+            elif orgidx == 1 and cnt >= 2:
                 bsIndexTrace.append(20)
-            elif subidx == 5 and cnt >= 2:
+            elif orgidx == 5 and cnt >= 2:
                 bsIndexTrace.append(10)
             else:
                 bsIndexTrace.append(0)
@@ -87,9 +102,7 @@ class MeasSetgen(object):
                 FrequencyHz.append(frequencyTable[i])
             
             self.df['TxFrequencyHz'] = FrequencyHz
-            
-            return self.df
-            
+                        
         except:
             print("Error: fn_freqidx2Hz")
         
@@ -100,11 +113,24 @@ class MeasSetgen(object):
     #     FrequencyHz.insert(n, func_freqidx2Hz(i))
     #     n += 1
     # df_sort['TxFrequencyHz'] = FrequencyHz
-
+    
+    def fn_dataout(self):
+        
+        ## group param에서 SUBMODEINDEX 추가하여 정렬 준비 및 정렬하기
+        sort_params = ['OrgIndex'] + self.group_params
+        df_sort = self.df.sort_values(by=sort_params, ascending=True).reset_index()
+        
+        ## data-out
+        df = df_sort
+        df.to_csv('./example/MeasSetGen/csv_files/check_20230127.csv')    
+        
 
 if __name__ == '__main__':
     cl_measset = MeasSetgen()
+    cl_measset.fn_merge_df()
+    cl_measset.fn_findOrgIdx()
     cl_measset.fn_bsIdx()
-    df = cl_measset.fn_freqidx2Hz()
+    cl_measset.fn_freqidx2Hz()
+    cl_measset.fn_dataout()
 
-    df.to_csv('./example/MeasSetGen/csv_files/check_20230126.csv')
+    
