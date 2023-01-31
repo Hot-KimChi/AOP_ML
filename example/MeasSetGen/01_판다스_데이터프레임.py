df3dcde90e99176ccb9cad72a310dd7555cf9234
InSeop Hwang <inseop.hwang@hotmail.com>
2023-01-31 18:04:19 +0900

    1) merge to main_class

--- example/MeasSetGen/01_판다스_데이터프레임.py
+++ example/MeasSetGen/01_판다스_데이터프레임.py
@@ -1,15 +1,29 @@
-import pandas as pd
-import numpy as np
-import warnings
-warnings.filterwarnings("ignore")
-
 import os
 import tkinter
 import pymssql
-from tkinter import filedialog
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 from tkinter import *
 from tkinter import ttk
+from functools import partial
 import configparser
+import warnings
+warnings.filterwarnings("ignore")
+
+from tkinter import filedialog
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import mean_absolute_error
+
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
 
 
 class SQL(object):
@@ -274,8 +288,6 @@ class MeasSetGen(object):
         self.fn_freqidx2Hz()
         self.fn_cnt_cycle()
         self.fn_dataout()
-        
-        
     
         
     def fn_loadfile(self):
