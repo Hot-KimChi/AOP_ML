6560a0e99d4fe4aa4ed4aef2cf4ed49ec7d5cccf
InSeop Hwang <inseop.hwang@hotmail.com>
2023-05-16 15:57:43 +0900

    update package files

---
+++ MeasSetGen_pkg/data_inout.py
@@ -0,0 +1,25 @@
+from tkinter import filedialog
+import pandas as pd
+
+class DataInOut():
+
+    def fn_loadfile(self):
+        ### 데이터 파일 읽어오기.
+        self.data = filedialog.askopenfilename(initialdir='.txt')
+        self.data = pd.read_csv(self.data, sep='\t', encoding='cp949')
+
+        return self.data
+
+
+    def fn_dataout(self, group_params, df):
+        ## group param에서 SUBMODEINDEX 추가하여 정렬 준비 및 정렬하기
+
+        self.group_params = group_params
+        self.df = df
+
+        sort_params = ['OrgBeamstyleIdx'] + self.group_params
+        df_sort = self.df.sort_values(by=sort_params, ascending=True).reset_index()
+
+        ## data-out
+        df = df_sort
+        df.to_csv('./csv_files/check_20230131.csv')
---
+++ MeasSetGen_pkg/meas_generation.py
@@ -0,0 +1,237 @@
+
+class MeasSetGen(object):
+
+    def __init__(self):
+        super().__init__()
+        self.initialize()
+
+
+    def initialize(self):
+        window_gen = tkinter.Toplevel()
+        window_gen.title(f"{database}" + ' / MeasSet_generation')
+        window_gen.geometry("600x200")
+        window_gen.resizable(False, False)
+
+        frame1 = Frame(window_gen, relief="solid", bd=2)
+        frame1.pack(side="top", fill="both", expand=True)
+
+        label_probename = Label(frame1, text='Probe Name')
+        label_probename.place(x=5, y=5)
+        self.combo_probename = ttk.Combobox(frame1, value=list_probe, height=0, state='readonly')
+        self.combo_probename.place(x=5, y=25)
+
+        btn_load = Button(frame1, width=15, height=2, text='Select & Load', command=self._fn_gen_sequence)
+        btn_load.place(x=200, y=5)
+
+        btn_insert = Button(frame1, width=15, height=2, text='To MS-SQL', command=self.fn_dataout)
+        btn_insert.place(x=350, y=5)
+
+        frame2 = Frame(window_gen, relief="solid", bd=2)
+        frame2.pack(side="bottom", fill="both", expand=True)
+
+        # Labels
+        label_DumpSW = Label(frame2, text="[dumpSwVersion]")
+        label_DumpSW.grid(row=0, column=0)
+
+        label_MaxVolt = Label(frame2, text="[maxTxVoltageVolt]")
+        label_MaxVolt.grid(row=2, column=0)
+
+        label_CeilVolt = Label(frame2, text="[ceilTxVoltageVolt]")
+        label_CeilVolt.grid(row=2, column=1)
+
+        label_TotalVoltpt = Label(frame2, text="[totalVoltagePt]")
+        label_TotalVoltpt.grid(row=2, column=2)
+
+        label_NumMeasVolt = Label(frame2, text="[numMeasVoltage]")
+        label_NumMeasVolt.grid(row=2, column=3)
+
+        # Entry boxes
+        self.box_DumpSW = Entry(frame2, justify='center')
+        self.box_DumpSW.grid(row=1, column=0)
+
+        self.box_MaxVolt = Entry(frame2, justify='center')
+        self.box_MaxVolt.grid(row=3, column=0)
+
+        self.box_CeilVolt = Entry(frame2, justify='center')
+        self.box_CeilVolt.grid(row=3, column=1)
+
+        self.box_TotalVoltpt = Entry(frame2, justify='center')
+        self.box_TotalVoltpt.grid(row=3, column=2)
+
+        self.box_NumMeasVolt = Entry(frame2, justify='center')
+        self.box_NumMeasVolt.grid(row=3, column=3)
+
+        window_gen.mainloop()
+
+
+    def _fn_gen_sequence(self):
+        self.fn_loadfile()
+        self.fn_select()
+        self.fn_merge_df()
+        self.fn_findOrgIdx()
+        self.fn_bsIdx()
+        self.fn_freqidx2Hz()
+        self.fn_cnt_cycle()
+        self.fn_calc_profvolt()
+        self.fn_zMeasNum()
+        self.fn_predictML()
+
+        self.fn_dataout()
+
+
+
+
+
+    ## find freq index
+    def fn_findOrgIdx(self):
+
+        orgindex = []
+
+        for mode, subidx in zip(self.df['MODE'], self.df['SUBMODEINDEX']):
+            if mode == 'B' and subidx == 0:
+                orgindex.append(0)
+            elif mode == 'B' and subidx == 1:
+                orgindex.append(1)
+            elif mode == 'Cb' and subidx == 0:
+                orgindex.append(5)
+            elif mode == 'D' and subidx == 0:
+                orgindex.append(10)
+            elif mode == 'M' and subidx == 0:
+                orgindex.append(15)
+            elif mode == 'M' and subidx == 1:
+                orgindex.append(20)
+
+        self.df['OrgBeamstyleIdx'] = orgindex
+
+    ## bsIndexTrace algorithm
+    def fn_bsIdx(self):
+
+        bsIndexTrace = []
+
+        for orgidx, cnt in zip(self.df['OrgBeamstyleIdx'], self.df['Count']):
+            if orgidx == 0 and cnt >= 2:
+                bsIndexTrace.append(15)
+            elif orgidx == 1 and cnt >= 2:
+                bsIndexTrace.append(20)
+            elif orgidx == 5 and cnt >= 2:
+                bsIndexTrace.append(10)
+            else:
+                bsIndexTrace.append(0)
+
+        self.df['bsIndexTrace'] = bsIndexTrace
+
+    ## FrequencyIndex to FrequencyHz
+    def fn_freqidx2Hz(self):
+        try:
+            frequencyTable = [1000000, 1111100, 1250000, 1333300, 1428600, 1538500, 1666700, 1818200, 2000000, 2222200,
+                              2500000, 2666700, 2857100, 3076900, 3333300, 3636400, 3809500, 4000000, 4210500, 4444400,
+                              4705900, 5000000, 5333300, 5714300, 6153800, 6666700, 7272700, 8000000, 8888900, 10000000,
+                              11428600, 13333333, 16000000, 20000000, 26666667, 11428600, 11428600, 11428600, 11428600,
+                              11428600,
+                              11428600, 11428600, 11428600, 11428600, 11428600, 11428600, 11428600, 11428600, 11428600]
+
+            FrequencyHz = []
+            for i in self.df['SYSTXFREQINDEX'].values:
+                FrequencyHz.append(frequencyTable[i])
+
+            self.df['TxFrequencyHz'] = FrequencyHz
+
+        except:
+            print("Error: fn_freqidx2Hz")
+
+    # n = 0
+    # FrequencyHz = []
+    # for i in df_sort['SYSTXFREQINDEX'].values:
+    #     FrequencyHz.insert(n, func_freqidx2Hz(i))
+    #     n += 1
+    # df_sort['TxFrequencyHz'] = FrequencyHz
+
+    ## Calc_cycle for RLE code
+    def fn_cnt_cycle(self):
+
+        list_cycle = []
+        for i in range(len(self.df['TXPGWAVEFORMSTYLE'])):
+            if self.df['TXPGWAVEFORMSTYLE'][i] == 0:
+                rle = self.df['TXPULSERLE'].str.split(":")[i]
+                list_flt = list(map(float, rle))
+                ## 아래 code도 가능.
+                ## floatList = [float(x) for x in list_option]
+                abs_value = np.abs(list_flt)
+
+                calc = []
+                for value in abs_value:
+                    if 1 < value:
+                        calc.append(round(value - 1, 4))
+                    else:
+                        calc.append(value)
+                cycle = round(sum(calc), 2)
+                list_cycle.append(cycle)
+
+            else:
+                cycle = self.df['PROBENUMTXCYCLES'][i]
+                list_cycle.append(cycle)
+
+        self.df['ProbeNumTxCycles'] = list_cycle
+
+    ## function: calc_profTxVoltage 구현
+    def fn_calc_profvolt(self):
+        try:
+            profTxVoltageVolt = []
+            for str_maxV, str_ceilV, str_totalpt in zip(self.df['maxTxVoltageVolt'], self.df['ceilTxVoltageVolt'],
+                                                        self.df['totalVoltagePt']):
+                idx = 2
+                ## tkinter에서 넘어오는 데이터 string.
+                maxV = float(str_maxV)
+                ceilV = float(str_ceilV)
+                totalpt = int(str_totalpt)
+
+                profTxVoltageVolt.append(round((min(maxV, ceilV)) ** ((totalpt - 1 - idx) / (totalpt - 1)), 2))
+
+            self.df['profTxVoltageVolt'] = profTxVoltageVolt
+
+        except:
+            print('error: func_profvolt')
+
+    ## function: calc zMeasNum 구현
+    def fn_zMeasNum(self):
+        try:
+            zStartDistCm = 0.5
+            zMeasNum = []
+
+            for focus in self.df['TXFOCUSLOCCM']:
+                if (focus <= 3):
+                    zMeasNum.append((5 - zStartDistCm) * 10)
+                elif (focus <= 6):
+                    zMeasNum.append((8 - zStartDistCm) * 10)
+                elif (focus <= 9):
+                    zMeasNum.append((12 - zStartDistCm) * 10)
+                else:
+                    zMeasNum.append((14 - zStartDistCm) * 10)
+
+            self.df['zMeasNum'] = zMeasNum
+
+        except:
+            print('error: func_zMeaNum')
+
+    def fn_predictML(self):
+        ## predict by Machine Learning model.
+        ## load modeling by pickle file.
+        loaded_model = joblib.load('Model/RandomForest_v1_python37.pkl')
+
+        ## take parameters for ML from measSet_gen file.
+        est_params = self.df[
+            ['TxFrequencyHz', 'TXFOCUSLOCCM', 'NUMTXELEMENTS', 'TXPGWAVEFORMSTYLE', 'ProbeNumTxCycles', 'ELEVAPERINDEX',
+             'ISTXCHANNELMODULATIONEN']]
+        connect = SQL(command=4)
+        est_geo = connect.fn_sql_get()
+
+        est_params[['probePitchCm']] = est_geo['probePitchCm']
+        est_params[['probeRadiusCm']] = est_geo['probeRadiusCm']
+        est_params[['probeElevAperCm0']] = est_geo['probeElevAperCm0']
+        est_params[['probeElevFocusRangCm']] = est_geo['probeElevFocusRangCm']
+
+        zt_est = loaded_model.predict(est_params)
+        df_zt_est = pd.DataFrame(zt_est, columns=['zt_est'])
+
+        self.df['zt_est'] = round(df_zt_est, 1)
+
