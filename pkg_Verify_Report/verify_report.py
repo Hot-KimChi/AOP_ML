2bc895592e616d18b254a739d44123447b9719f7
InSeop Hwang <inseop.hwang@hotmail.com>
2023-09-22 21:14:44 +0900

    update verify

--- pkg_Verify_Report/verify_report.py
+++ pkg_Verify_Report/verify_report.py
@@ -9,7 +9,7 @@ from pkg_Viewer.select_param import SelectParam
 
 class Verify_Report:
     """
-    verify step same as intial viewer
+    verify step same as initial viewer
     """
     def __init__(self, database, list_probe):
 
