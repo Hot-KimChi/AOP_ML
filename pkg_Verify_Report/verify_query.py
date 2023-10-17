7375228d0529c126fc8f94abaa46d00dc2f82854
InSeop Hwang <inseop.hwang@hotmail.com>
2023-10-17 18:16:28 +0900

    1) Done - database.py modify 2) in progress - verify_query.py modify

--- pkg_Verify_Report/verify_query.py
+++ pkg_Verify_Report/verify_query.py
@@ -5,5 +5,9 @@ class verify_query:
     For verification report, execute query to MS-SQL
 
     """
-    def __init__(self, sorted_param, seledted_measSSId, report_term, selected_probeId):
-        self.sort
+    def __init__(self, sorted_param, selected_measSSId, report_term, selected_probeId):
+        self.sorted_param = sorted_param,
+        self.selected_measSSId = selected_measSSId,
+        self.report_term = report_term,
+        self.selected_probeId = selected_probeId
+
