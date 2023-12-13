b8a6283791d9c1b39ca026c22aa37af09f50ad3d
InSeop Hwang <inseop.hwang@hotmail.com>
2023-12-13 18:02:50 +0900

    JSX update for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          AOP team generation 입니다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>AOP Team</h4>
+      </div>
+
     </div>
   );
 }
