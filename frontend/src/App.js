ad998574e8a20d5d6bdeb383f765a726b97acb2c
InSeop Hwang <inseop.hwang@hotmail.com>
2024-02-27 21:06:33 +0900

    update frontend frame

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let server_list = ["K2", "Juniper"];
+  useState
+  
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>AOP MeasSetGeneration</h4>
+
+        <h3>{server_list}</h3>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
