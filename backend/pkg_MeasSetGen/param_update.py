0d4c816a68071c8b88b7921e3485d8f308233ff2
InSeop Hwang <inseop.hwang@hotmail.com>
2024-06-04 23:39:34 +0900

    update groupindex algorithm

--- backend/pkg_MeasSetGen/param_update.py
+++ backend/pkg_MeasSetGen/param_update.py
@@ -38,7 +38,7 @@ class ParamUpdate:
         3) 
         
         """
-       
+
         
         ##### B & M mode process #####
         df_B_mode = df.loc[(df['Mode'] == 'B')]
@@ -54,7 +54,6 @@ class ParamUpdate:
         df_total_BM = df_total_BM.fillna(0)
         
         
-        
         # GroupIndex 열 생성
         group_index = 1
         group_indices = []
@@ -68,44 +67,19 @@ class ParamUpdate:
             prev_value = value
         df_total_BM['GroupIndex'] = group_indices
 
-        # 첫 번째 조건의 중복 제거
-        group_params = ['IsTxChannelModulationEn', 'SysTxFreqIndex', 'TxpgWaveformStyle', 'ProbeNumTxCycles', 
-                        'TxPulseRle', 'ElevAperIndex', 'IsPresetCpaEn']
-        data_no_duplicates = df_total_BM.drop_duplicates(subset=group_params, keep='first')
-
-
-        # 두 번째 조건의 중복 제거 (GroupIndex에 따라 NumTxElements가 동일할 경우)
-        def remove_group_duplicates(df):
-            # GroupIndex별로 그룹핑
-            grouped = df.groupby('GroupIndex')
-            # 중복 제거된 데이터프레임을 저장할 리스트
-            unique_rows = []
-            for _, group in grouped:
-                # 모든 NumTxElements 값이 동일한지 확인
-                if group['NumTxElements'].nunique() == 1:
-                    continue  # 중복된 그룹은 제외
-                unique_rows.extend(group.to_dict('records'))
-            return pd.DataFrame(unique_rows)
-
-        # 적용
-        df_total = remove_group_duplicates(data_no_duplicates)  
         
-        
+        # 중복 열 제거
+        cols_to_drop = ['SysTxFreqIndex', 'TxpgWaveformStyle', 'ProbeNumTxCycles', 'IsTxChannelModulationEn', 'IsPresetCpaEn', 'ElevAperIndex']
+        df_total_BM = df_total_BM.drop_duplicates(subset=cols_to_drop, keep='first')
 
-    #     ## groupby count 를 위해 parameter setting
-    #     group_params_TxElement = ['IsTxChannelModulationEn', 'SysTxFreqIndex', 'TxpgWaveformStyle', 'ProbeNumTxCycles', 
-    #                             'TxPulseRle', 'ElevAperIndex', 'IsPresetCpaEn', 'TxFocusLocCm', 'NumTxElements'] 
-    #     ## 중복된 column 갯수 세기 --> 중복된 열 삭제됨.
-    #     dup_count = df_total_BM.groupby(group_params_TxElement, as_index=False).size()
-
-    #   # 중복된 열 제거, 위쪽에 갯수와 동일하게 하기 위해, 동일하게 정열. / 중복된 열 갯수를 df_total에 집어넣기.
-    #     df_total = df_total_BM.drop_duplicates(subset=group_params_TxElement, keep='first')
-        
-        
-    #     # 그룹별로 행 수를 계산하여 'Count' 열에 추가
-    #     df_total['Count'] = df_total[group_params_TxElement].merge(dup_count, on=group_params_TxElement, how='left')['size']
-    #     df_total = df_total.sort_values(by=self.sortParam, ascending=True).reset_index(drop=True)
-
+        # NumTxElements가 GroupIndex에 따라 전체 데이터가 동일할 경우에만 중복 열 제거
+        # NumTxElements가 GroupIndex에 따라 전체 데이터가 동일할 경우 중복된 행 중 첫 번째 행만 유지
+        df_total_BM = df_total_BM.drop_duplicates(subset=['NumTxElements', 'GroupIndex'], keep='first')
+        # grouped = df_total_BM.groupby('GroupIndex')['NumTxElements'].nunique().reset_index()
+        # duplicate_groups = grouped[grouped['NumTxElements'] == 1]['GroupIndex'].tolist()
+        # df_total = df_total_BM[~df_total_BM['GroupIndex'].isin(duplicate_groups)]
+        df_total = df_total_BM
+           
         ## Test code
         df_total.to_csv('test.csv')        
 
