9c43d6dd1f84c4697099301eb3012ba7b5b065cf
InSeop Hwang <inseop.hwang@hotmail.com>
2024-06-14 16:30:59 +0900

    update duplicated algorithm

--- backend/pkg_MeasSetGen/meas_generation.py
+++ backend/pkg_MeasSetGen/meas_generation.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
 
 from pkg_MeasSetGen.data_inout import loadfile
@@ -15,50 +14,57 @@ class MeasSetGen:
     1) select & Load 버튼: _get_sequence 함수 실행
     2) To MS-SQL / To Excel / protocol check
     """
+
     def __init__(self, database, list_probe):
 
         self.database = database
         self.list_probe = list_probe
 
         self.window = tk.Toplevel()
-        self.window.title(f"{self.database}" + ' / MeasSet_generation')
+        self.window.title(f"{self.database}" + " / MeasSet_generation")
         self.window.geometry("600x200")
         self.window.resizable(False, False)
 
-        frame = Frame(self.window, relief="solid", bd=2)
+        frame = tk.Frame(self.window, relief="solid", bd=2)
         frame.pack(side="top", fill="both", expand=True)
 
-        label_probename = Label(frame, text='Probe Name')
+        label_probename = tk.Label(frame, text="Probe Name")
         label_probename.place(x=5, y=5)
-        self.combo_probe = ttk.Combobox(frame, value=self.list_probe, height=0, state='readonly')
+        self.combo_probe = ttk.Combobox(
+            frame, value=self.list_probe, height=0, state="readonly"
+        )
         self.combo_probe.place(x=5, y=25)
 
-        btn_load = Button(frame, width=15, height=2, text='Select & Load', command=self._get_sequence)
+        btn_load = tk.Button(
+            frame, width=15, height=2, text="Select & Load", command=self._get_sequence
+        )
         btn_load.place(x=200, y=5)
 
-        btn_insert = Button(frame, width=15, height=2, text='To MS-SQL', command=loadfile)
+        btn_insert = tk.Button(
+            frame, width=15, height=2, text="To MS-SQL", command=loadfile
+        )
         btn_insert.place(x=350, y=5)
-
 
     def _get_sequence(self):
 
         ## probename / probeid 로 구분
-        self.probe = self.combo_probe.get().replace(" ", "")
-        idx = self.probe.find("|")
+        probe = self.combo_probe.get().replace(" ", "")
+        idx = probe.find("|")
         if idx >= 0:
-            probename = self.probe[:idx]
-            probeid = self.probe[idx + 1:]
+            probename = probe[:idx]
+            probeid = probe[idx + 1 :]
 
-
-        ## 파일 선택할 수 있는 algorithm
+        ## 파일 선택할 수 있는 algorithm / 중복 데이터 삭제 및 group_index
         raw_data = loadfile()
-
-        ## 동일한 데이터 삭제
-        param_update = ParamUpdate(raw_data)                                    ## 클래스 인스턴스 생성
-        self.selected_df, self.group_params = param_update.param_merge()        ## 메서드 호출 및 반환된 값 저장.
+        param_update = ParamUpdate(raw_data)  ## 클래스 인스턴스 생성
+        self.selected_df, self.group_params = (
+            param_update.param_merge()
+        )  ## 메서드 호출 및 반환된 값 저장.
 
         ## 선택한 데이터를 기반으로 parameter 생성.
-        self.gen_df = ParamGen(data=self.selected_df, probeid=probeid, probename=probename)
+        self.gen_df = ParamGen(
+            data=self.selected_df, probeid=probeid, probename=probename
+        )
 
         ## predictML for intensity case
         ML = PredictML(self.gen_df.df, probeid)
@@ -67,12 +73,18 @@ class MeasSetGen:
         ## 클래스 인스턴스를 데이터프레임으로 변환 / DataOut 클래스 이용하여 csv 파일로 추출.
         df = self.gen_df
 
-        dataout = DataOut(case=0, database=self.database, probe=self.probe, df1=df, group_params=self.group_params)
+        dataout = DataOut(
+            case=0,
+            database=self.database,
+            probe=probe,
+            df1=df,
+            group_params=self.group_params,
+        )
         dataout.make_dir()
         dataout.save_excel()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     gen_window = tk.Tk()
     app_gen = MeasSetGen(gen_window)
 
--- backend/pkg_MeasSetGen/param_update.py
+++ backend/pkg_MeasSetGen/param_update.py
@@ -8,6 +8,7 @@ class ParamUpdate:
     """
 
     def __init__(self, df):
+
         self.df = df
 
         ## parameter selection
@@ -30,19 +31,13 @@ class ParamUpdate:
         ]
         self.selected_df = self.df.loc[:, list_param]
 
-        ## sorting 은 안하는 것으로 결정. --> UE에서 #F에 따른 데이터를 구분하지 않기에.
+        ## sorting 은 안하는 것으로 결정.
+        # --> UE에서 #F에 따른 데이터를 구분하지 않기에.
 
     ## B / C / D / M 모드 구분하여 중복 삭제하고 난 후, merge.
     def param_merge(self):
 
         df = self.selected_df
-
-        """
-        1) focus 갯수를 파악하여 group index count --> 정렬하기 / 포커스가 올라가다가 내려가면 그룹인덱스가 다음 인덱스로 넘어감.
-        2) 만약에 UE에서 중복이 된 condition을 request하면 어떻게 하는가? --> 나중에 해결
-        3) 
-        
-        """
 
         ##### B & M mode process #####
         df_B_mode = df.loc[(df["Mode"] == "B")]
@@ -51,10 +46,10 @@ class ParamUpdate:
         df_D_mode = df.loc[df["Mode"] == "D"]
         df_CEUS_mode = df.loc[df["Mode"] == "Contrast"]
 
-        ## B and M-mode에서 중복된 부분 삭제 --> NumTxElement까지 포함하여 삭제하기.
-        df_total_BM = pd.concat([df_B_mode, df_M_mode])
-        df_total_BM = df_total_BM.reset_index(drop=True)
-        df_total_BM = df_total_BM.fillna(0)
+        # B and M-mode 데이터만 선택하여 데이터프레임으로 생성
+        df_BM = pd.concat([df_B_mode, df_M_mode])
+        df_BM = df_BM.reset_index(drop=True)
+        df_BM = df_BM.fillna(0)
 
         # 중복 열 제거
         cols_to_drop = [
@@ -66,53 +61,52 @@ class ParamUpdate:
             "ElevAperIndex",
             "NumTxElements",
         ]
-        df_total_BM = df_total_BM.drop_duplicates(subset=cols_to_drop, keep="first")
+
+        # 중복 개수 열 추가
+        df_BM = df_BM.drop_duplicates(subset=cols_to_drop, keep="first")
+
+        duplicated_mask = df_BM.duplicated(subset=cols_to_drop, keep=False)
+        duplicated_mask.to_csv("dup.csv")
+        df_BM["Duplicate_Count"] = duplicated_mask.map({True: 1, False: 0})
+
+        # Duplicate_Count가 1이고 Mode가 'M'인 행 삭제 / Count_group_index를 데이터프레임에 추가
+        df_BM = df_BM[(df_BM["Duplicate_Count"] != 1) | (df_BM["Mode"] != "M")]
+        df_BM = self.countGroupIdx(df_BM)
+
+        ## C and D-mode 데이터만 선택하여 데이터프레임으로 생성
+        df_CD = pd.concat([df_C_mode, df_D_mode])
+        df_CD = df_CD.reset_index(drop=True)
+        df_CD = df_CD.fillna(0)
+
+        # 중복 개수 열 추가
+        df_CD = df_CD.drop_duplicates(subset=cols_to_drop, keep="first")
+        duplicated_mask = df_CD.duplicated(subset=cols_to_drop, keep=False)
+        df_CD["Duplicate_Count"] = duplicated_mask.map({True: 1, False: 0})
+
+        # Duplicate_Count가 1이고 Mode가 'D'인 행 삭제 / Count_group_index를 데이터프레임에 추가
+        df_CD = df_CD[(df_CD["Duplicate_Count"] != 1) | (df_CD["Mode"] != "D")]
+        df_CD = self.countGroupIdx(df_CD)
+
+        df_total = pd.concat([df_BM, df_CD, df_CEUS_mode])
 
         ## Test code
-        df_total_BM.to_csv("test_BM.csv")
+        df_total.to_csv("test.csv")
+
+        return df_total, group_params
+
+    def countGroupIdx(self, df):
 
         # GroupIndex 열 생성
         group_index = 1
         group_indices = []
         prev_value = None
-        for value in df_total_BM["TxFocusLocCm"]:
+        for value in df["TxFocusLocCm"]:
             if prev_value is None or value >= prev_value:
                 group_indices.append(group_index)
             else:
                 group_index += 1
                 group_indices.append(group_index)
             prev_value = value
-        df_total_BM["GroupIndex"] = group_indices
+        df["groupIndex"] = group_indices
 
-        df_total = df_total_BM
-        ## Test code
-        df_total.to_csv("test.csv")
-
-        #     ## 4개 모드 데이터프레임 합치기 / 합쳐진 데이터프레임 index reset
-        #     ## 데이터 Null --> [0]으로 변환(데이터의 정렬, groupby null 값 문제 발생)
-        #     ## 1) df_B = df_M 일 경우, M 삭제
-        #     # B_numTxElements = list(df_B_mode['NumTxElements'])
-        #     # M_numTxElements = list(df_M_mode['NumTxElements'])
-        #     # BC_numTxElements = list(df_C_mode['NumTxElements'])
-        #     # D_numTxElements = list(df_D_mode['NumTxElements'])
-
-        #     # if B_numTxElements == M_numTxElements:
-        #     df_total = pd.concat([df_B_mode, df_M_mode, df_C_mode, df_D_mode, df_CEUS_mode])
-
-        #     df_total = df_total.reset_index(drop=True)
-        #     df_total = df_total.fillna(0)
-
-        #     ## groupby count 를 위해 parameter setting
-        #     group_params = ['IsTxChannelModulationEn', 'ProbeNumTxCycles', 'SysTxFreqIndex', 'TxpgWaveformStyle',
-        #                    'TxPulseRle', 'ElevAperIndex', 'IsPresetCpaEn', 'TxFocusLocCm']
-
-        #     ## 중복된 column 갯수 세기 --> 중복된 열 삭제됨.
-        #     dup_count = df_total.groupby(group_params, as_index=False).size()
-
-        #     ## 중복된 열 제거, 위쪽에 갯수와 동일하게 하기 위해, 동일하게 정열.
-        #     ## 중복된 열 갯수를 df_total에 집어넣기.
-        #     df_total = df_total.drop_duplicates(subset=group_params, keep='first')
-        #     df_total = df_total.sort_values(by=group_params, ascending=True).reset_index()
-        #     df_total['Count'] = dup_count['size']
-
-        return df_total, group_params
+        return df
